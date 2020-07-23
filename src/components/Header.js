54155517ee0d7008537bf135bc08659cab706ffa
trapintrovert <mcneo062@gmail.com>
2020-07-23 16:00:36 +0100

    Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
 
 import Hamburger from './Hamburger'
 
-const Header = () => {
+const Header = ({history}) => {
+
+  const [state, setState ] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu"
+  })
+
+  const [disabled, setDisabled] = useState(false)
+  
+  //use effect for page changes
+  history.listen(() => {
+    setState({ clicked: false, menuName: 'Menu'})
+  })
+  
+  const handleMenu = () => {
+    disabledMenu();
+    if(state.initial === false){
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: 'Close'
+      })
+      
+    }else if(state.clicked === true){
+      setState({
+        clicked: !state.clicked,
+        menuName: 'Menu'
+      })
+    
+    }else if(state.clicked === false){
+      setState({
+        clicked: !state.clicked,
+        menuName: 'Close'
+      })
+      
+    }
+  }
+
+  // Determine if our menu should be disabled;
+
+  const disabledMenu = () =>{
+    setDisabled(!disabled)
+    setTimeout(() => {
+      setDisabled(false)
+    }, 1200)
+  }
   return (
     <header>
       <div className="container">
@@ -13,14 +59,14 @@ const Header = () => {
               <Link to='/'>HAMBURG</Link>
             </div>
             <div className="menu">
-              <button>Menu</button>
+              <button disabled={disabled} onClick={handleMenu}>Menu</button>
             </div>
           </div>
         </div>
       </div>
-      <Hamburger/>
+      <Hamburger state={state}/>
     </header>
   );
 };
 
-export default Header;
+export default withRouter(Header);
